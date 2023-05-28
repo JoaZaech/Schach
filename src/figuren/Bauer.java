7e0a2a00ca3b2a1667a0e3f8ac52f6ebded6a9c7
Rafa-L2 <134836587+Rafa-L2@users.noreply.github.com>
2023-05-28 13:00:48 +0200

    boolean[][] zeigeLaufmoeglichkeiten

--- src/figuren/Bauer.java
+++ src/figuren/Bauer.java
@@ -16,7 +16,7 @@ public class Bauer extends Figur{
     }
 
     @Override
-    public void zeigeLaufmoeglichkeiten() {
+    public boolean[][] zeigeLaufmoeglichkeiten() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
