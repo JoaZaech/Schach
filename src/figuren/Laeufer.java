d10d566e19382ef32a41c8fed1f7650de0cd0856
Zäch <60199908+scxryshop@users.noreply.github.com>
2023-05-28 15:36:19 +0200

    Laeufer test

--- src/figuren/Laeufer.java
+++ src/figuren/Laeufer.java
@@ -30,7 +30,26 @@ public class Laeufer extends Figur{
             }
         }
         
+        int[][] arr = {{-1,-1}, {-1,1}, {1,-1}, {1,1}};
+        
+        for (int i = 0; i < arr[0].length; i++) {
+            for(int[] a : arr){
+                
+            }
+        }
+        
         return laufMoeglichkeiten;
+    }
+    
+    public boolean aufSchachbrett(int x, int y){
+        boolean aufSchachbrett = true;
+        if(x < 0 || x >= 8){
+            aufSchachbrett = false;
+        }
+        if(y < 0 || y >= 8){
+            aufSchachbrett = false;
+        }
+        return aufSchachbrett;
     }
     
 }
