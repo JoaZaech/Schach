a45dfbe091659f92cd8eb87ddb2dd5278212e623
Zäch <joalukas12@gmail.com>
2023-05-30 17:51:40 +0200

    Dame fertig + Schachdetektor Klasse

--- src/figuren/Dame.java
+++ src/figuren/Dame.java
@@ -1,6 +1,10 @@
 
 package figuren;
 
+import static schach.Brett.BRETT_HEIGHT;
+import static schach.Brett.BRETT_WIDTH;
+import schach.Steuerung;
+
 public class Dame extends Figur{
 
     public Dame(int pReihe, int pSpalte, boolean pWeiss) {
@@ -9,7 +13,126 @@ public class Dame extends Figur{
 
     @Override
     public void berechneLaufmoeglichkeiten() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        dasBrett = Steuerung.dasBrett;
+         resetLaufmoeglichkeiten();
+        //  Links 
+        for (int i = reihe - 1; i >= 0; i--) {
+            if (dasBrett.FigurAufBrett(i, spalte)) {
+                if (dasBrett.gibFigur(i, spalte).isWeiss() == weiss) {
+                    break;
+                } else {
+                    laufMoeglichkeiten[i][spalte] = true;
+                    break;
+                }
+            } else {
+                laufMoeglichkeiten[i][spalte] = true;
+            }
+        }
+
+        // Rechts
+        for (int i = reihe + 1; i < BRETT_WIDTH; i++) {
+            if (dasBrett.FigurAufBrett(i, spalte)) {
+                if (dasBrett.gibFigur(i, spalte).isWeiss() == weiss) {
+                    break;
+                } else {
+                    laufMoeglichkeiten[i][spalte] = true;
+                    break;
+                }
+            } else {
+                laufMoeglichkeiten[i][spalte] = true;
+            }
+        }
+
+        //  Oben 
+        for (int i = spalte - 1; i >= 0; i--) {
+            if (dasBrett.FigurAufBrett(reihe, i)) {
+                if (dasBrett.gibFigur(reihe, i).isWeiss() == weiss) {
+                    break;
+                } else {
+                    laufMoeglichkeiten[reihe][i] = true;
+                    break;
+                }
+            } else {
+                laufMoeglichkeiten[reihe][i] = true;
+            }
+        }
+
+        // Unten
+        for (int i = spalte + 1; i < BRETT_HEIGHT; i++) {
+            if (dasBrett.FigurAufBrett(reihe, i)) {
+                if (dasBrett.gibFigur(reihe, i).isWeiss() == weiss) {
+                    break;
+                } else {
+                    laufMoeglichkeiten[reihe][i] = true;
+                    break;
+                }
+            } else {
+                laufMoeglichkeiten[reihe][i] = true;
+            }
+        }
+        int spaltezaehler = spalte;
+        int reihezaehler = reihe;
+        dasBrett = Steuerung.dasBrett;
+        for (int a = 0; a < 8; a++) { // rechts unten
+            spaltezaehler++;
+            reihezaehler++;
+            if(pruefeAlles(reihezaehler,spaltezaehler) == false){
+                break;
+            }
+        }
+        spaltezaehler = spalte;
+        reihezaehler = reihe;
+        for (int a = 0; a < 8; a++) { // rechts oben
+            spaltezaehler--;
+            reihezaehler++;
+            if(pruefeAlles(reihezaehler,spaltezaehler) == false){
+                break;
+            }
+        }
+        spaltezaehler = spalte;
+        reihezaehler = reihe;
+        for (int a = 0; a < 8; a++) { // links oben
+            spaltezaehler--;
+            reihezaehler--;
+            if(pruefeAlles(reihezaehler,spaltezaehler) == false){
+                break;
+            }
+        }
+        spaltezaehler = spalte;
+        reihezaehler = reihe;
+        for (int a = 0; a < 8; a++) { // links unten
+            spaltezaehler++;
+            reihezaehler--;
+            if(pruefeAlles(reihezaehler,spaltezaehler) == false){
+                break;
+            }
+        }
+    }
+    public boolean pruefeAlles(int x, int y){
+        if(aufSchachbrett(x,y)){
+            if( dasBrett.FigurAufBrett(x,y) == false ){
+                laufMoeglichkeiten[x][y] = true;
+            }else{
+                if( dasBrett.gibFigur(x, y).isWeiss() != weiss ){
+                    laufMoeglichkeiten[x][y] = true;
+                }
+                return false;
+            }
+        }else{
+            return false;
+        }
+        return true;
+    }
+
+    public boolean aufSchachbrett(int x, int y) {
+        boolean aufSchachbrett = true;
+        if (x < 0 || x >= 8) {
+            aufSchachbrett = false;
+        }
+        if (y < 0 || y >= 8) {
+            aufSchachbrett = false;
+        }
+        return aufSchachbrett;
     }
     
 }
---
+++ src/schach/Schachdetektor.java
@@ -0,0 +1,4 @@
+package schach;
+public class Schachdetektor {
+    // Pruefe auf Schach
+}
