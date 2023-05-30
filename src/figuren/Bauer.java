fdf2ba06dcdf9d3e5d8e55f380e443d644cde206
Rafa-L2 <134836587+Rafa-L2@users.noreply.github.com>
2023-05-30 14:04:22 +0200

    Randbauer kann schlagen
    Fehlerbehebung, Randbauer kann jetzt schlagen

--- src/figuren/Bauer.java
+++ src/figuren/Bauer.java
@@ -23,18 +23,20 @@ public class Bauer extends Figur{
 
         if(!dasBrett.FigurAufBrett(reihe, spalte+richtung)){ // ein Feld vor
             laufMoeglichkeiten[reihe][spalte+richtung] = true;
+            if(spalte+richtung*2 >= 0 && spalte+richtung*2 <= BRETT_HEIGHT){ // zwei Felder vor
+                if((spalte == 1 || spalte == 6) && !dasBrett.FigurAufBrett(reihe, spalte+richtung*2)){
+                    laufMoeglichkeiten[reihe][spalte+richtung*2] = true;
+                } 
+            }
         }
-        if(spalte+richtung*2 >= 0 && spalte+richtung*2 <= BRETT_HEIGHT){ // zwei Felder vor
-            if((spalte == 1 || spalte == 6) && !dasBrett.FigurAufBrett(reihe, spalte+richtung*2)){
-                laufMoeglichkeiten[reihe][spalte+richtung*2] = true;
-            } 
-        }
-        if(reihe-1 >= 0 && reihe+1 <= BRETT_HEIGHT){ //schlagen
+        if(reihe+1 <= BRETT_HEIGHT){ //schlagen
             if( dasBrett.FigurAufBrett(reihe+1, spalte+richtung)){
                 if( dasBrett.gibFigur(reihe+1, spalte+richtung).isWeiss() != weiss){
                     laufMoeglichkeiten[reihe+1][spalte+richtung] = true;
                 }
             }
+        }
+        if(reihe-1 >= 0){
             if(dasBrett.FigurAufBrett(reihe-1, spalte+richtung)){
                 if(dasBrett.gibFigur(reihe-1, spalte+richtung).isWeiss() != weiss){
                     laufMoeglichkeiten[reihe-1][spalte+richtung] = true;
