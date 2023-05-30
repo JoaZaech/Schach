afbf7194d89ec9852c06c514862c48467dffebbf
Rafa-L2 <134836587+Rafa-L2@users.noreply.github.com>
2023-05-30 13:57:27 +0200

    Laeufer laufen
    laeuefer laufen, Code kann man vielleicht kuerzen

--- src/figuren/Laeufer.java
+++ src/figuren/Laeufer.java
@@ -13,7 +13,7 @@ public class Laeufer extends Figur {
     public void berechneLaufmoeglichkeiten() {
         dasBrett = Steuerung.dasBrett;
         resetLaufmoeglichkeiten();
-
+        /*
         // AbstandRe
         int rowRe = Brett.BRETT_WIDTH - reihe + 1;
 
@@ -24,25 +24,77 @@ public class Laeufer extends Figur {
         int columnLi = Brett.BRETT_HEIGHT - columnRe + 1;
 
         // Rechts unten
-        for (int i = 1; i < rowRe; i++) {
-            for (int j = 1; j < columnRe; j++) {
-                if (j == i && aufSchachbrett(reihe + j, spalte + i)) {
+        for (int spaltezaehler = 1; spaltezaehler < rowRe; spaltezaehler++) {
+            for (int reihezaehler = 1; reihezaehler < columnRe; reihezaehler++) {
+                if (reihezaehler == spaltezaehler && aufSchachbrett(reihe + reihezaehler, spalte + spaltezaehler)) {
 
-                    laufMoeglichkeiten[reihe + j][spalte + i] = true;
+                    laufMoeglichkeiten[reihe + reihezaehler][spalte + spaltezaehler] = true;
                 }
             }
         }
 
         // Links unten
-        for (int i = 1; i < rowLi + 1; i++) {
-            for (int j = 1; j < columnLi + 1; j++) {
-                if (j == i && aufSchachbrett(reihe - j, spalte - i)) {
-                    laufMoeglichkeiten[reihe - j][spalte - i] = true;
+        for (int spaltezaehler = 1; spaltezaehler < rowLi + 1; spaltezaehler++) {
+            for (int reihezaehler = 1; reihezaehler < columnLi + 1; reihezaehler++) {
+                if (reihezaehler == spaltezaehler && aufSchachbrett(reihe - reihezaehler, spalte - spaltezaehler)) {
+                    laufMoeglichkeiten[reihe - reihezaehler][spalte - spaltezaehler] = true;
                 }
             }
         }
-        
-        
+        */
+        int spaltezaehler = spalte;
+        int reihezaehler = reihe;
+        dasBrett = Steuerung.dasBrett;
+        for (int a = 0; a < 8; a++) { // rechts unten
+            spaltezaehler++;
+            reihezaehler++;
+            if(pruefeAlles(reihezaehler,spaltezaehler) == false){
+                break;
+            }
+        }
+        spaltezaehler = spalte;
+        reihezaehler = reihe;
+        for (int a = 0; a < 8; a++) { // rechts oben
+            spaltezaehler--;
+            reihezaehler++;
+            if(pruefeAlles(reihezaehler,spaltezaehler) == false){
+                break;
+            }
+        }
+        spaltezaehler = spalte;
+        reihezaehler = reihe;
+        for (int a = 0; a < 8; a++) { // links oben
+            spaltezaehler--;
+            reihezaehler--;
+            if(pruefeAlles(reihezaehler,spaltezaehler) == false){
+                break;
+            }
+        }
+        spaltezaehler = spalte;
+        reihezaehler = reihe;
+        for (int a = 0; a < 8; a++) { // links unten
+            spaltezaehler++;
+            reihezaehler--;
+            if(pruefeAlles(reihezaehler,spaltezaehler) == false){
+                break;
+            }
+        }
+    }
+    
+    public boolean pruefeAlles(int x, int y){
+        if(aufSchachbrett(x,y)){
+            if( dasBrett.FigurAufBrett(x,y) == false ){
+                laufMoeglichkeiten[x][y] = true;
+            }else{
+                if( dasBrett.gibFigur(x, y).isWeiss() != weiss ){
+                    laufMoeglichkeiten[x][y] = true;
+                }
+                return false;
+            }
+        }else{
+            return false;
+        }
+        return true;
     }
 
     public boolean aufSchachbrett(int x, int y) {
