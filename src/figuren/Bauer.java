79268e4e8cb1034023569a7f0890f5a15e30d97d
Rafa-L2 <134836587+Rafa-L2@users.noreply.github.com>
2023-06-13 09:06:39 +0200

    Bauer kann bis zum Ende laufen
    keine Fehlermeldung wenn der Bauer bis zum Ende laueft

--- src/figuren/Bauer.java
+++ src/figuren/Bauer.java
@@ -13,35 +13,36 @@ public class Bauer extends Figur{
     @Override
     public void berechneLaufmoeglichkeiten() {
         dasBrett = Steuerung.dasBrett;
-         resetLaufmoeglichkeiten();
+        resetLaufmoeglichkeiten();
         int richtung;
         if(weiss == true){
             richtung = -1;
         }else{ 
             richtung = 1;
         }
-
-        if(!dasBrett.FigurAufBrett(reihe, spalte+richtung)){ // ein Feld vor
-            laufMoeglichkeiten[reihe][spalte+richtung] = true;
-            if(spalte+richtung*2 >= 0 && spalte+richtung*2 <= BRETT_HEIGHT){ // zwei Felder vor
-                if(( spalte == 1 && weiss == false || spalte == 6 && weiss == true ) && !dasBrett.FigurAufBrett(reihe, spalte+richtung*2)){
-                    laufMoeglichkeiten[reihe][spalte+richtung*2] = true;
+        if(spalte+richtung >= 0 && spalte+richtung <= BRETT_HEIGHT){
+            if( !dasBrett.FigurAufBrett(reihe, spalte+richtung)){ // ein Feld vor
+                laufMoeglichkeiten[reihe][spalte+richtung] = true;
+                //if(spalte+richtung*2 >= 0 && spalte+richtung*2 <= BRETT_HEIGHT){ // zwei Felder vor
+                    if(( spalte == 1 && weiss == false || spalte == 6 && weiss == true ) && !dasBrett.FigurAufBrett(reihe, spalte+richtung*2)){
+                        laufMoeglichkeiten[reihe][spalte+richtung*2] = true;
+                    } 
+                //}
+            }
+            if(reihe+1 <= BRETT_HEIGHT){ //schlagen
+                if( dasBrett.FigurAufBrett(reihe+1, spalte+richtung)){
+                    if( dasBrett.gibFigur(reihe+1, spalte+richtung).isWeiss() != weiss){
+                        laufMoeglichkeiten[reihe+1][spalte+richtung] = true;
+                    }
+                }
+            }
+            if(reihe-1 >= 0){ //schlagen andere Richtung
+                if(dasBrett.FigurAufBrett(reihe-1, spalte+richtung)){
+                    if(dasBrett.gibFigur(reihe-1, spalte+richtung).isWeiss() != weiss){
+                        laufMoeglichkeiten[reihe-1][spalte+richtung] = true;
+                    }
                 } 
             }
-        }
-        if(reihe+1 <= BRETT_HEIGHT){ //schlagen
-            if( dasBrett.FigurAufBrett(reihe+1, spalte+richtung)){
-                if( dasBrett.gibFigur(reihe+1, spalte+richtung).isWeiss() != weiss){
-                    laufMoeglichkeiten[reihe+1][spalte+richtung] = true;
-                }
-            }
-        }
-        if(reihe-1 >= 0){
-            if(dasBrett.FigurAufBrett(reihe-1, spalte+richtung)){
-                if(dasBrett.gibFigur(reihe-1, spalte+richtung).isWeiss() != weiss){
-                    laufMoeglichkeiten[reihe-1][spalte+richtung] = true;
-                }
-            } 
         }
     }
     
